9c8acfe2cc728a41b8fd34a2b58fce8446e4b69a
Somnathghosal <drsomnathghosal@gmail.com>
2025-05-04 12:44:19 +0530

    Update Research.tsx

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -38,7 +38,7 @@ const researchData: ResearchItem[] = [
       "Public policy emphasizes inclusive healthcare and education for rural communities.",
     details:
       "Public policy emphasizes inclusive healthcare and education for rural communities. Decentralized healthcare models improve accessibility and affordability, ensuring better health outcomes. Technology and community participation play a crucial role in enhancing rural education opportunities. Addressing gaps in policy implementation leads to evidence-based solutions for sustainable development. Action research focuses on innovative frameworks that integrate education, health, and livelihood strategies.",
-  videoUrl: "https://www.youtube-nocookie.com/embed/q3VDMkTZf1Y?si=MXknN4KVRirW9Kkn&amp;start=1",
+  videoUrl: "https://www.youtube.com/embed/q3VDMkTZf1Y?si=tdwjo1hfB7yJKI8n",
   },
   {
     title: "Social Forestry and Nature-Based Entrepreneurship",
